9c2e91b88a2c197bd1e8a905b0292d6dc141fb98
“Albert” <arambamalbert@gmail.com>
2023-08-04 12:47:48 +0530

    delte file changes

--- deleteProduct.php
+++ deleteProduct.php
@@ -3,24 +3,21 @@
 include 'connection.php';
 
 
-// if (isset($_GET['ID'])) {
-//    $ID = $_GET['ID'];
+if (isset($_GET['ID'])) {
+   $ID = $_GET['ID'];
 
   
-// $sql = "DELETE FROM Products WHERE ID = $ID";
-//    $result = mysqli_query($conn, $sql);
-//    if ($result) {
-//       header('location:MainProduct.php');
-//    }
-// }
+$sql = "DELETE FROM Products WHERE ID = $ID";
+   $result = mysqli_query($conn, $sql);
+   if ($result) {
+      header('location:MainProduct.php');
+   }
+}
 
 if($_POST['type']==1){
    $id=$_POST['id'];
    $sql = "DELETE FROM Products WHERE ID in ($id)";
    $result = mysqli_query($conn, $sql);
-   if ($result) {
-            header('location:MainProduct.php');
-         }
-   mysqli_close($conn);
+   
 }
 ?>
